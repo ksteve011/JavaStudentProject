df5dbb85469c1cec6d3cdcf4aed5082fc4ab76d1
Khalela Stevens <khalelau.stevens@student.fairfield.edu>
2020-12-19 19:31:03 -0500

    new

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/JavaStudentProject/index.html'));
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
